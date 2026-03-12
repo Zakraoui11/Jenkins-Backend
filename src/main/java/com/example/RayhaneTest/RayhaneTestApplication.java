d87297e5b8f1546d330885ea78646daf7c7ea9fe
Rayhane <rayhanzakraoui@gmail.com>
2026-03-12 17:26:40 +0100

    first spring boot project

---
+++ src/main/java/com/example/RayhaneTest/RayhaneTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.RayhaneTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RayhaneTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RayhaneTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RayhaneTest/RayhaneTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RayhaneTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RayhaneTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
